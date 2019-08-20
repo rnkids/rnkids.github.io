5908f51d020a74bf85b55889b40f76ceef12901f
rnkids <lequysang@yahoo.com>
2019-08-20 19:15:36 +0700

    add counter

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const App = () => <div>Hello</div>
+const { useState } = React
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+const App = () => {
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+            <div>Counter from {count}</div>
+            <button onClick={() => setCount(count + 1)}> +1 </button>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
